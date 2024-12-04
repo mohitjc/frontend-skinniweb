16f8ec47d454b76006e52cc7ed65af80d09ae487
Rajni Bala <bala@jcsoftwaresolution.com>
2024-12-04 12:07:23 +0530

    secret key

--- src/Pages/reducers/index.js
+++ src/Pages/reducers/index.js
@@ -1,21 +1,36 @@
 /*********** Reduceres defined here *********/
 
 import { persistCombineReducers } from 'redux-persist';
-import storage from 'redux-persist/es/storage'; // default: localStorage if web, AsyncStorage if react-native
-// import storageSession from 'redux-persist/lib/storage/session'
+import storage from 'redux-persist/es/storage/session';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+
 // reducers
 import user from './modules/user';
 import loader from './modules/loader';
 import search from './modules/search';
+import sitedetail from './modules/sitedetail';
+import subscription from './modules/subscription';
+import environment from '../../environment';
+
+// Encryption transform
+const encryptTransformF = encryptTransform({
+    secretKey: environment.secretKey,  
+    onError: function (error) {
+        console.error('Encryption error:', error);
+    },
+});
 
 const userPersistConfig = {
-    key: 'admin-app',
+    key: 'skinniWeb-app',
     storage: storage,
     blacklist: ['loader'],
+    transforms: [encryptTransformF],
 };
 
 export default persistCombineReducers(userPersistConfig, {
     loader,
     user,
-    search
+    search,
+    sitedetail,
+    subscription
 });
