af1c670e4b1ad67210640addfc524ca071190ff4
Rajni Bala <bala@jcsoftwaresolution.com>
2024-12-04 15:09:08 +0530

    changes

--- src/components/global/PageLayout/index.js
+++ src/components/global/PageLayout/index.js
@@ -81,7 +81,7 @@ const PageLayout = ({ children }) => {
     { name: "Home", url: "/" },
     { name: "Blogs", url: "/blogs" },
     { name: "About", url: "/about" },
-    { name: "Plan", url: "/plan" }
+    // { name: "Plan", url: "/plan" }
   ];
   // if (user.loggedIn) {
   //   menus.push({ name: "Plan", url: "/plan" });
@@ -298,11 +298,6 @@ const PageLayout = ({ children }) => {
             <div className="grid grid-cols-12 gap-4">
               <div className="col-span-12 lg:col-span-4">
                 <div>
-                  {/* <img
-                    className="w-full  object-cover max-w-28"
-                    src="/assets/img/logo.png"
-                    alt=""
-                  /> */}
                   <img src={methodModel.noImg(sitedetail?.logo)} width="80" />
                   <p className="text-gray-300 my-8 w-1/2 text-sm">
                     In the fast-paced world of modern technology, many
@@ -312,22 +307,11 @@ const PageLayout = ({ children }) => {
                   <ul className="flex items-center">
                     {sitedetail && sitedetail?.socialMedia?.map((item, index) => (
                       <>
-                    <li className="border border-gray-300 p-2 rounded-3xl w-9	 h-9 text-center cursor-pointer hover:opacity-[80%] group mr-2">
-                      {/* <FaFacebookF className="text-white" /> */}
+                    <li className="border border-gray-300 p-2 rounded-3xl w-9	 h-9 text-center cursor-pointer hover:opacity-[80%] group mr-2"> 
                       <a href={item.link} target="_blank" rel="noopener noreferrer"><img src={methodModel.noImg(item?.icon)} width="120" className="rounded-full" /></a>
-                    </li>
-                    {/* {item.name} */}
+                    </li> 
                     </>
-                    ))}
-                    {/* <li className="border border-gray-300 p-2 rounded-3xl w-9	 h-9 text-center cursor-pointer hover:bg-blue-500 group ms-3">
-                      <FaDribbble className="text-white" />
-                    </li>
-                    <li className="border border-gray-300 p-2 rounded-3xl w-9	 h-9 text-center cursor-pointer hover:bg-blue-500 group ms-3">
-                      <FaGithub className="text-white" />
-                    </li>
-                    <li className="border border-gray-300 p-2 rounded-3xl w-9	 h-9 text-center cursor-pointer hover:bg-blue-500 group ms-3">
-                      <SlSocialTwitter className="text-white" />
-                    </li> */}
+                    ))} 
                   </ul>
                 </div>
               </div>
@@ -374,34 +358,7 @@ const PageLayout = ({ children }) => {
                       </li>
                       
                     </ul>
-                  </div>
-                  {/* <div className="col-span-12 lg:col-span-4">
-                    <h2 className="text-white font-bold text-lg mb-8">
-                      Community
-                    </h2>
-                    <ul>
-                      <li className=" text-gray-300 group">
-                        <p className="text-gray-300 group-hover:text-white cursor-pointer mb-2">
-                          Learners
-                        </p>
-                      </li>
-                      <li className=" text-gray-300 group">
-                        <p className="text-gray-300 group-hover:text-white cursor-pointer mb-2">
-                          Leadership
-                        </p>
-                      </li>
-                      <li className=" text-gray-300 group">
-                        <p className="text-gray-300 group-hover:text-white cursor-pointer mb-2">
-                          Partners
-                        </p>
-                      </li>
-                      <li className=" text-gray-300 group">
-                        <p className="text-gray-300 group-hover:text-white cursor-pointer">
-                          Developers
-                        </p>
-                      </li>
-                    </ul>
-                  </div> */}
+                  </div> 
                   <div className="col-span-12 lg:col-span-4">
                     <h2 className="text-white font-bold text-lg mb-8">
                       Newsletter
