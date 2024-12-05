601ee74d05240704913d16dc383a98b3df98019e
Rishu Kumar <rishuk@jcsoftwaresolution.com>
2024-12-05 10:11:04 +0530

    changes design

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -6,6 +6,11 @@ import loader from "../../methods/loader";
 import "./profile.scss";
 import methodModel from "../../methods/methods";
 import { useSelector } from "react-redux";
+import { MdEdit } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaCalendarAlt } from "react-icons/fa";
+import { TbGenderBigender } from "react-icons/tb";
 
 const Profile = () => {
   const user = useSelector((state: any) => state.user);
@@ -202,8 +207,97 @@ const Profile = () => {
     //   </div>
     // </Layout>
     <div className="">
-      <div className="container">
-      
+              <div className="bg_color_profile">
+              <div className="container">
+            <div className="bg_profile">
+
+              <div className="logo_profile">
+          <img src="/assets/img/Skinnii-Logo.webp"/>
+          </div>
+
+        </div>
+        <div className="">
+      <div className="profile_main">
+          <div className="profile_upload">
+            <img src="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/linkedin-profile-picture-maker/dummy_image/thumb/004.webp"/>
+            <MdEdit className="edit_icon" />
+          </div>
+
+          <div className="my_profile">
+            <div className="mb-5">
+            <div className="header_frofile mb-5">
+            <h2>MY PROFILE</h2>
+            <div className="edit_info">
+            <MdEdit/> Edit Info
+            </div>
+            </div>
+          <div className="row">
+              <div className="col-md-3">
+                <div className="card_input">
+                  <input className="form-control" placeholder="Full Name"></input>
+              <FaUser />
+              </div>
+              </div>
+              <div className="col-md-3">
+                <div className="card_input">
+                  <input className="form-control" placeholder="E-Mail"></input>
+              <MdEmail />
+              </div>
+              </div>
+              <div className="col-md-3">
+                <div className="card_input">
+                  <input className="form-control" placeholder="dd/mm/yyy"></input>
+              <FaCalendarAlt />
+              </div>
+              </div>
+              <div className="col-md-3">
+                <div className="card_input">
+                  <input className="form-control" placeholder="Gender"></input>
+              <TbGenderBigender />
+              </div>
+              </div>
+          </div>
+          </div>
+          
+          <div className="">
+          <div className="header_frofile mb-5">
+            <h2>MY GOALS</h2>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+              <div className="card_box">
+                <div className="card_text mb-2">
+                <img src="/assets/img/profileimg1.png"/>
+                  <h3>100 Pound</h3>
+                </div>
+                <p>Current Weight in Pound</p>
+              </div>
+              </div>
+              <div className="col-md-4">
+              <div className="card_box">
+                <div className="card_text mb-2">
+                <img src="/assets/img/profileimg2.png"/>
+                  <h3>100 Pound</h3>
+                </div>
+                <p>Current Height in Pound</p>
+              </div>
+              </div>
+              <div className="col-md-4">
+              <div className="card_box">
+                <div className="card_text mb-2">
+                <img src="/assets/img/profileimg3.png"/>
+                  <h3>60 Pound</h3>
+                </div>
+                <p>Goal Weight in Pound</p>
+              </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+        </div>
+        </div>
+      </div>
       </div>
     </div>
   );
