7c10c8c17045517be4f5d6ba7a1a5db8bbee2cb5
Rishu Kumar <rishuk@jcsoftwaresolution.com>
2025-01-14 11:27:49 +0530

    changes design

--- src/Pages/MyPayments/index.jsx
+++ src/Pages/MyPayments/index.jsx
@@ -1,5 +1,7 @@
 import Layout from "../../components/sidebarglobal/layout";
 import { FaLongArrowAltDown } from "react-icons/fa";
+import { BsQuestionCircleFill } from "react-icons/bs";
+
 
 const Payments = () => {
   return (
@@ -55,42 +57,41 @@ const Payments = () => {
            <div className="bg-[#FFF1E7] shadow-[0px_5px_8px_-2px_#c4c4c4] p-[1.5rem] rounded-[12px]">
            <div className="bg-white p-[1.5rem] rounded-[12px]">
             <h2 className="border-b font-[600] text-[18px] pb-2 mb-5">Add A Credit Card</h2>
-
-            <div>
-            <h3 className="border-b pb-2 mb-4">Cardholder Information</h3>
+            <div className="mb-5">
+            <h3 className="border-b text-[18px] pb-2 mb-4">Cardholder Information</h3>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">First Name<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Last Name<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Company</label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div>
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Phone Number<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
             </div>
 
             <div>
-            <h3 className="border-b pb-2 mb-4">Cardholder Information</h3>
+            <h3 className="border-b text-[18px] pb-2 mb-4">Cardholder Information</h3>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Street<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white mt-2"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white mt-2"/>
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">City<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">State/Province<span class="text-[#FF0028]">*</span></label>
                <div class="relative z-20">
-            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
+            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
@@ -101,12 +102,12 @@ const Payments = () => {
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Zip/Postal Code<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div className="mb-3">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Country<span class="text-[#FF0028]">*</span></label>
                <div class="relative z-20">
-            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
+            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
@@ -124,18 +125,23 @@ const Payments = () => {
                <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mb-3">
                <div className="">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">CC Number<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
                </div>
                <div className="">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">CCV<span class="text-[#FF0028]">*</span></label>
-               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+              <div className="relative">
+               <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"/>
+               <span class="absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 ">
+               <BsQuestionCircleFill className="text-[#828282]" />
+               </span>
+              </div>
                </div>
                </div>
                <div className="">
                <label class="block mb-2 text-[12px] font-medium text-gray-900 dark:text-white">Expiration<span class="text-[#FF0028]">*</span></label>
                <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
                <div class="relative z-20">
-            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
+            <select class="relative bg-gray-50 border border-gray-300 text-gray-900 text-[12px] block w-full p-2.5 pr-5 pr-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white appearance-none">
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
                <option value="" class="dark:bg-dark-2">Option</option>
