7885b2f0ba2956481d5c6a74e4caf5de3289aa4c
Rishu Kumar <rishuk@jcsoftwaresolution.com>
2025-01-20 17:36:45 +0530

    changes design

--- src/components/sidebarglobal/sidebar/Html.jsx
+++ src/components/sidebarglobal/sidebar/Html.jsx
@@ -204,7 +204,7 @@ const Html = ({ ListItemLink, tabclass, isAllow, route, isOpen, user , Logout}) 
                                 <NavLink
                                   to={itm.url}
                                   className={(isActive) =>
-                                    "relative p-2.5 rounded-l-full flex items-center gap-[12px] text-sm font-normal text-[#000] hover:text-[#000] hover:bg-[#00000014] !no-underline " +
+                                    "relative p-2.5 rounded-l-full flex items-center gap-[12px] text-sm font-normal text-[#000] hover:text-[#000] hover:bg-[#8282821f] !no-underline " +
                                     (location?.pathname === itm.url &&
                                       "relative rounded_div !text-[#FEE4D0] !bg-[#828282] hover:!text-[#FEE4D0] !font-medium")
                                   }
@@ -250,7 +250,7 @@ const Html = ({ ListItemLink, tabclass, isAllow, route, isOpen, user , Logout}) 
                 <NavLink
                   to={"/login"}
                   className={(isActive) =>
-                    "relative p-2.5 rounded-l-full flex items-center gap-[12px] text-sm font-normal text-[#000] hover:text-[#000] hover:bg-[#00000014] !no-underline transition-all " +
+                    "relative p-2.5 rounded-l-full flex items-center gap-[12px] text-sm font-normal text-[#000] hover:text-[#000] hover:bg-[#8282821f] !no-underline transition-all " +
                     (location?.pathname === "" &&
                       "relative rounded_div !text-[#FEE4D0] !bg-[#828282] hover:!text-[#FEE4D0] !font-medium")
                   }
