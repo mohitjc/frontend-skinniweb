03e26f9afd61930b0facd818d23ff385ab9122df
vishal <sharmavish@jcsoftwaresolution.com>
2025-01-28 18:22:43 +0530

    set frums page

--- src/Pages/Chats/index.jsx
+++ src/Pages/Chats/index.jsx
@@ -5,16 +5,20 @@ const Chats = () => {
   
   return (
     <Layout>
-      <div className="bg-white px-[1rem] py-[1.5rem] sm:p-[2rem] rounded-[12px]">
-        <div className="bg-[#FFF1E7] shadow-[0px_5px_8px_-2px_#c4c4c4] px-[1rem] py-[1.5rem] sm:p-[2rem] rounded-[12px] mb-[1.5rem] sm:mb-[2.5rem]">
-          <div className="flex flex-wrap justify-between gap-y-3 gap-x-5 mb-2">
-            <div className="">
-              <h1 className="text-[22px] font-bold mb-1">Chats</h1>
-              {/* <p className="text-sm text-[#828282]">1 item</p> */}
-            </div>
-          </div>
-          
-        </div>
+      <div className="bg-white  rounded-[12px]">
+      <div className="flex items-center justify-between bg-[#FEE4D0]">
+<div className="flex items-center p-2">
+<img className="w-[40px] h-[40px] rounded-full object-cover" src="/assets/img/profile-image.jpg"></img>
+<div className="ml-1">
+  <p className="">Manny Lorem</p>
+  <p className="">last seen 5 mins ago</p>
+</div>
+</div>
+
+<div className="flex items-center">
+
+</div>
+      </div>
 
         
       </div>
