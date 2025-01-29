57587c0d4b834c2fc75f18539c8fd360b6bb3097
vishal <sharmavish@jcsoftwaresolution.com>
2025-01-29 10:25:17 +0530

    chat design

--- src/Pages/Chats/index.jsx
+++ src/Pages/Chats/index.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
 import Layout from "../../components/sidebarglobal/layout";
 import { IoMdSearch } from "react-icons/io";
+import { LuPhone } from "react-icons/lu";
+import { IoVideocamOutline } from "react-icons/io5";
 
 const Chats = () => {
   
   return (
     <Layout>
       <div className="bg-white  rounded-[12px]">
-      <div className="flex items-center justify-between bg-[#FEE4D0]">
-<div className="flex items-center p-2">
+      <div className="flex items-center justify-between bg-[#FEE4D0] p-2">
+<div className="flex items-center ">
 <img className="w-[40px] h-[40px] rounded-full object-cover" src="/assets/img/profile-image.jpg"></img>
-<div className="ml-1">
-  <p className="">Manny Lorem</p>
-  <p className="">last seen 5 mins ago</p>
+<div className="ml-2">
+  <p className="text-[14px] font-[600] text-[#828282]">Manny Lorem</p>
+  <p className="text-[12px] font-[400] text-[#707991]">last seen 5 mins ago</p>
+  
 </div>
 </div>
 
 <div className="flex items-center">
-<IoMdSearch />
+<IoMdSearch className="mr-3" />
+<LuPhone className="mr-3" />
+<IoVideocamOutline className="mr-3" />
 
 </div>
       </div>
